8b5df29bac8bc32e28d0b059e660d9b9c2291ede
mashirozx <moezhx@outlook.com>
2022-02-10 10:26:47 +0800

    fix: local only and content type passing to composer in edit mode

--- db/schema.rb
+++ db/schema.rb
@@ -869,6 +869,8 @@ ActiveRecord::Schema.define(version: 2022_08_27_195229) do
     t.text "media_descriptions", array: true
     t.string "poll_options", array: true
     t.boolean "sensitive"
+    t.string "content_type"
+    t.boolean "local_only"
     t.index ["account_id"], name: "index_status_edits_on_account_id"
     t.index ["status_id"], name: "index_status_edits_on_status_id"
   end
