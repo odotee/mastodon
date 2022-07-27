51fa7a92334605b63eb4388e8c41ddd41b7372d1
mashirozx <moezhx@outlook.com>
2022-07-27 17:03:16 +0800

    feat: change how logo paths were controlled inside rails application

--- app/helpers/branding_helper.rb
+++ app/helpers/branding_helper.rb
@@ -7,6 +7,8 @@ module BrandingHelper
       _logo_as_symbol_icon
     when :wordmark
       _logo_as_symbol_wordmark
+    when :wordmark_with_sub
+      suffix != '' ? _logo_as_symbol_wordmark_with_sub : _logo_as_symbol_wordmark
     end
   end
 
@@ -14,24 +16,34 @@ module BrandingHelper
     content_tag(:svg, tag(:use, href: '#logo-symbol-wordmark'), viewBox: '0 0 261 66', class: 'logo logo--wordmark')
   end
 
+  def _logo_as_symbol_wordmark_with_sub
+    content_tag(:svg, tag(:use, href: '#logo-symbol-wordmark-with-sub'), viewBox: '0 0 261 66', class: 'logo logo--wordmark')
+  end
+
   def _logo_as_symbol_icon
     content_tag(:svg, tag(:use, href: '#logo-symbol-icon'), viewBox: '0 0 79 79', class: 'logo logo--icon')
   end
 
   def render_logo
-    image_pack_tag('logo.svg', alt: 'Mastodon', class: 'logo logo--icon')
+    image_pack_tag("logo#{suffix}.svg", alt: 'Mastodon', class: 'logo logo--icon')
   end
 
   def render_symbol(version = :icon)
     path = begin
       case version
       when :icon
-        'logo-symbol-icon.svg'
+        "logo-symbol-icon#{suffix}.svg"
       when :wordmark
-        'logo-symbol-wordmark.svg'
+        "logo-symbol-wordmark#{suffix}.svg"
+      when :wordmark_with_sub
+        "logo-symbol-wordmark#{suffix != '' ? '-with-sub' : ''}#{suffix}.svg"
       end
     end
 
     render(file: Rails.root.join('app', 'javascript', 'images', path)).html_safe # rubocop:disable Rails/OutputSafety
+  end
+
+  def suffix
+    ENV['LOGO_PACK'] || ''
   end
 end
